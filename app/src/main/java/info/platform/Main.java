cb67c7949873e3791f006c576e33f5c76148e5da
solitarynew <1348288404@qq.com>
2022-11-06 18:50:16 +0800

    refactor

--- app/src/main/java/info/platform/Main.java
+++ app/src/main/java/info/platform/Main.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphqlSpringBootApplication {
-
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(GraphqlSpringBootApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
-
 }
